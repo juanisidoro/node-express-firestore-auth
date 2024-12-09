b5341c018af843068b4bd92edcaf3e399c187eb3
juanisidoro <juan.isidoro.gc@gmail.com>
2024-12-09 20:14:21 +0100

    server modificado

--- src/infrastructure/server/server.ts
+++ src/infrastructure/server/server.ts
@@ -2,7 +2,8 @@
 import app from "./app";
 import { ENV } from "../config/env";
 
-const PORT = ENV.PORT; // usar la variable centralizada
+// Priorizar process.env.PORT en producción y usar ENV.PORT como respaldo
+const PORT = process.env.PORT || ENV.PORT;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
