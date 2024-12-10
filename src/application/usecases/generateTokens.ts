ed9cb6074252b894f00e45bafb45ff66d58b7773
juanisidoro <juan.isidoro.gc@gmail.com>
2024-12-10 19:00:54 +0100

    update

--- src/application/usecases/generateTokens.ts
+++ src/application/usecases/generateTokens.ts
@@ -1,11 +1,13 @@
 import { TokenService } from "../services/tokenService";
+import { Role } from "../../domain/entities/user"; // Importa el tipo Role
 
 export class GenerateTokens {
   constructor(private tokenService: TokenService) {}
 
-  async execute(email: string) {
-    const accessToken = this.tokenService.generateAccessToken(email);
+  async execute(email: string, role: Role) {
+    const accessToken = this.tokenService.generateAccessToken(email, role);
     const refreshToken = await this.tokenService.generateRefreshToken(email);
     return { accessToken, refreshToken };
   }
 }
+
